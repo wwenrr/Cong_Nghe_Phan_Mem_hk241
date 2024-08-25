20c35f54b2f4686b580a69c53a1dd05a76f66dee
MK <qscvdefb@gmail.com>
2024-08-25 18:19:56 +0700

    .

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,24 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './assets/components/nav';
-import Xu_huong from './assets/components/xu-huong';
-import Trang_suc from './assets/components/trang-suc';
-import Khuyen_mai from './assets/components/khuyen-mai';
-import Thuong_hieu from './assets/components/thuong-hieu';
+// import Xu_huong from './assets/components/xu-huong';
+// import Trang_suc from './assets/components/trang-suc';
+// import Khuyen_mai from './assets/components/khuyen-mai';
+// import Thuong_hieu from './assets/components/thuong-hieu';
+
+
 
 function App() {
+  const [point, setPoint] = useState(0);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Nav />}>
-          <Route path="/" element={<Xu_huong />} />
-          <Route path="/trang-suc" element={<Trang_suc />} />
-          <Route path="/thuong-hieu" element={<Thuong_hieu />} />
-          <Route path="/khuyen-mai" element={<Khuyen_mai />} />
+        <Route path="/" element={<Nav point={point} setPoint={setPoint}/>}>
+          {/* <Route path="/" element={<Xu_huong setPoint={setPoint}/>} /> */}
+          {/* <Route path="/trang-suc" element={<Trang_suc setPoint={setPoint}/>} />
+          <Route path="/thuong-hieu" element={<Thuong_hieu setPoint={setPoint}/>} />
+          <Route path="/khuyen-mai" element={<Khuyen_mai setPoint={setPoint}/>} /> */}
         </Route>
       </Routes> 
     </Router>
--- app/src/assets/components/nav.jsx
+++ app/src/assets/components/nav.jsx
@@ -1,51 +1,54 @@
 import React, { useRef } from 'react';
-import styles from '../css/nav.module.css'
 import phone from '../img/gg_phone.svg'
 import account from '../img/account.svg'
 import shop from '../img/shop.svg'
 import search from '../img/search.svg'
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, generatePath } from 'react-router-dom';
+import style from '../css/nav.module.scss'
 
-function Nav() {
+function Nav({ point, setPoint }) {
     const inputRef = useRef(null)
+    setPoint(0)
 
     return (
-        <div>
-            <nav className={styles.nav}>
-                <div className={styles.layer1}>
-                    <div className={styles.group1}>
-                        <div className={styles.phone}>
+        <>
+            <div>
+                <nav className={style.nav}>
+                    <layer1 className={style.layer1}>
+                        <div className={style.phone_gr}>
                             <img src={phone} alt="" />
-                            <div className="">1800-XX-XX-XX</div>
+                            <div className={style.num}>1800-XX-XX-XX</div>
                         </div>
+                        <div className={style.logo}>LOGO</div>
+                        <div className={style.cta}>
+                            <img src={account} alt="" />
+                            <img src={shop} alt="" />
+                        </div>
+                    </layer1>
 
-                        <div className={styles.logo}>LOGO</div>
+                    <div className={style.layer2}>
+                        <ul className={style.cta}>
+                            <Link>Xu hướng</Link>
+                            <Link>Trang sức</Link>
+                            <Link>Thương hiệu</Link>
+                            <Link>Khuyến mãi</Link>
+                        </ul>
+                        
+                        <div className={style.search_box}>
+                            <input type="text" name="" placeholder='Tìm kiếm' ref={inputRef}/>
+                            <img src={search} alt="" 
+                                onClick={() => {
+                                    inputRef.current.focus()
+                                }}
+                            />
+                        </div>
                     </div>
+                </nav>
 
-                    <div className={styles.group2}>
-                        <img src={account} alt="" />
-                        <img src={shop} alt="" />
-                    </div>
-                </div>
 
-                <div className={styles.layer2}>
-                    <ul className={styles.group1}>
-                        <Link to="/">Xu hướng</Link>
-                        <Link to="/trang-suc">Trang sức</Link>
-                        <Link to="/thuong-hieu">Thương hiệu</Link>
-                        <Link to="/khuyen-mai">Khuyến mãi</Link>
-                    </ul>
-
-                    <div className={styles.search}>
-                        <input type="text" placeholder='Tìm kiếm' ref={inputRef} />
-                        <img src={search} alt="" onClick={() => inputRef.current.focus()} />
-                    </div>
-
-                </div>
-            </nav>
-
+            </div>
             <Outlet />
-        </div>
+        </>
     )
 }
 
